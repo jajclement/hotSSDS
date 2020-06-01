5815429c115fa2079dcbca69050264a53569956a
Kevin <kevbrick@gmail.com>
2020-06-01 08:47:04 -0400

    Update config for nextflow/0.30.2

--- tests/testSSDS1.8.sh
+++ tests/testSSDS1.8.sh
@@ -1,7 +1,7 @@
 module load nextflow/0.30.2
 
 nextflow run -c $NXF_PIPEDIR/config.nf -profile local \
-    $NXF_PIPEDIR/SSDSPipeline_1.8.groovy \
+    $NXF_PIPEDIR/SSDSPipeline_1.8.nf \
     --fq1 $NXF_PIPEDIR/tests/fastq/ssdsLong.100k.R1.fastq \
     --fq2 $NXF_PIPEDIR/tests/fastq/ssdsLong.100k.R2.fastq \
     --r1Len 36 \
