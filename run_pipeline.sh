c11779d34e1ec032c1bb6e92916b833733eb8252
Pauline Auffret <pauline.auffret@igh.cnrs.fr>
2020-08-14 13:36:00 +0000

    Update run_pipeline.sh

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -2,13 +2,14 @@
 
 ANALYSIS_NAME="TEST"
 INPUT="tests/fastq/*{R1,R2}.fastq"
+GENOME="mm10"
 JOBNAME='SSDS_main'
 
 eval "$(conda shell.bash hook)"
 conda activate nextflow-dev
 
 sbatch -p computepart -J ${JOBNAME} --export=ALL --mem 5G -t 5-0:0 --mem-per-cpu=1000 \
-	--wrap "nextflow run mains.nf -c conf/igh.config --fqdir ${INPUT} --name ${ANALYSIS_NAME}"
+	--wrap "nextflow run mains.nf -c conf/igh.config --fqdir ${INPUT} --name ${ANALYSIS_NAME} --genome ${GENOME}"
 
 conda deactivate
 
