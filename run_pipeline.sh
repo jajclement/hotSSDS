8481269754d599c5bece088a76da857e4cf866aa
Pauline Auffret <pauline.auffret@igh.cnrs.fr>
2020-08-21 10:05:08 +0000

    Update run_pipeline.sh

--- run_pipeline.sh
+++ run_pipeline.sh
@@ -9,7 +9,7 @@ eval "$(conda shell.bash hook)"
 conda activate nextflow-dev
 
 sbatch -p computepart -J ${JOBNAME} --export=ALL --mem 5G -t 5-0:0 --mem-per-cpu=1000 \
-	--wrap "nextflow run mains.nf -c conf/igh.config --fqdir ${INPUT} --name ${ANALYSIS_NAME} --genome ${GENOME}"
+	--wrap "nextflow run main.nf -c conf/igh.config --fqdir ${INPUT} --name ${ANALYSIS_NAME} --genome ${GENOME}"
 
 conda deactivate
 
